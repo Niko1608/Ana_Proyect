ed4cae7e89f287fad469fcf001d7d3a71671ce92
Nicolas Cortes <nicolas.corgom@gmail.com>
2023-11-29 14:39:06 -0500

    Organized_Views_V2

--- src/features/auth/router/AuthRouter.tsx
+++ src/features/auth/router/AuthRouter.tsx
@@ -3,16 +3,27 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { AuthRoutes, AuthRoutesProps } from './types'
 import { Login } from '../login'
 import { Register } from '../register'
-import Start from '../start/Start'
+import { Start } from '../start/index'
 
 const Stack = createNativeStackNavigator<AuthRoutesProps>()
 
 const AuthRouter = () => (
   <Stack.Navigator>
-    <Stack.Screen name={AuthRoutes.Login} component={Login} options={{title: 'Login',headerTitleAlign: 'center'}}/>
-    <Stack.Screen name={AuthRoutes.Register} component={Register} options={{title: 'Register',headerTitleAlign: 'center'}} />
-    <Stack.Screen name={AuthRoutes.Start} component={Start} options={{title: 'Start',headerTitleAlign: 'center'}}/>
-
+    <Stack.Screen
+      name={AuthRoutes.Login}
+      component={Login}
+      options={{ title: 'Login', headerTitleAlign: 'center' }}
+    />
+    <Stack.Screen
+      name={AuthRoutes.Register}
+      component={Register}
+      options={{ title: 'Register', headerTitleAlign: 'center' }}
+    />
+    <Stack.Screen
+      name={AuthRoutes.Start}
+      component={Start}
+      options={{ title: 'Start', headerTitleAlign: 'center' }}
+    />
   </Stack.Navigator>
 )
 
